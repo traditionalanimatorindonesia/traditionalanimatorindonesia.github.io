fa8704821eead3cb3782589ce34aa084219f09cc
Sade <39307011+spikysaurus@users.noreply.github.com>
2025-10-14 00:25:25 +0700

    update

---
+++ assets/javascript/meta.js
@@ -0,0 +1,11 @@
+let metaString = `
+  
+<meta charset="UTF-8">
+<meta property="og:title" content="Traditional Animator Indonesia" />
+<meta property="og:description" content="Traditional Animator Indonesia adalah online group dimana kita memposting karya Animasi hand-drawn (Frame by Frame)" />
+<meta property="og:image" content="https://traditionalanimatorindonesia.github.io/assets/img/icon.png" />
+<base target="_blank">
+
+`;
+
+document.head.innerHTML += metaString;
